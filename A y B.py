007352b5a19992addc596cb2cf0908592382ead6
Cesarmc-0 <montealegrecesar45@gmail.com>
2025-07-07 13:32:02 -0500

    Update A y B.py
    tercer cambio

--- A y B.py
+++ A y B.py
@@ -1,7 +1,7 @@
 def compareTriplets(a, b):
     alice = bob = 0  # Puntuaciones iniciales
 
-    for i in range(3):  # Recorremos ambas listas en paralelo
+    for i in range(min(len(a),len(b))):  # Recorremos ambas listas en paralelo
         if a[i] > b[i]:
             alice += 1
         elif a[i] < b[i]:
