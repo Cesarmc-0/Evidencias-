d316166f9cf42922a4a1cc00b4b0bc1a29b5f9c7
Cesarmc-0 <montealegrecesar45@gmail.com>
2025-07-07 15:32:42 -0500

    Update A y B.py
    cambio definitivo

--- A y B.py
+++ A y B.py
@@ -1,10 +1,13 @@
 def compareTriplets(a, b):
-    alice = bob = 0  # Puntuaciones iniciales
-
-    for i in range(min(len(a),len(b))):  # Recorremos ambas listas en paralelo
-        if a[i] > b[i]:
+    maxlen = max(len(a), len(b))  # Obtenemos la longitud máxima de las dos listas
+    alice = 0
+    bob = 0  # Puntuaciones iniciales
+    for i in range(maxlen):  # Recorremos ambas listas en paralelo
+        ai = a[i] if i < len(a) else 0
+        bi = b[i] if i < len(b) else 0 # cuando falten elementos este se remplaza a cero
+        if ai > bi:
             alice += 1
-        elif a[i] < b[i]:
+        elif ai < bi:
             bob += 1
         # si son iguales, no hacemos nada
-    return alice, bob
+    return alice, bob  # Devolvemos las puntuaciones como una tupla
